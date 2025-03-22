2ea2ebe8808e9e6d866ef632539075c578a5e798
Ravinder <ravinder729090@gmail.com>
2025-03-22 12:50:39 +0530

    add user and captain login/signup components; implement routing in App.jsx

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <div id='home-container'>
+            <div id='home-container' className=''>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/5/58/Uber_logo_2018.svg" alt="uber-logo" className='uber-logo' />
                 <div className='sub-container'>
                     <h2>Get Started with Uber</h2>
